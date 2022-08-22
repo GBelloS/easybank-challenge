b772d71b6d3f06aaf6dcd30a9a893d991dedf61e
Gabriel Bello Schwarzbold <gabriel.b.schwarzbold@gmail.com>
2022-08-22 17:35:58 -0300

    <img> tags creation.

--- previewer/previewer.js
+++ previewer/previewer.js
@@ -4,7 +4,23 @@ async function getImages()
 {
     const response = await fetch('images.json')
     const imagesNames = await response.json()
-    console.log(imagesNames)
+
+    if(!Array.isArray(imagesNames))
+        return Promise.reject("JSON isn't an array")
+    
+    for(let i=0;i<imagesNames.length;i++)
+        imagesNames[i] = 'images/'+imagesNames[i]
+
+    return imagesNames
 }
 
-getImages()//.catch(()=>console.log('goes wrong'))
+function imgCreation(images)
+{
+    const body = document.body
+    for(img of images)
+        body.appendChild(document.createElement('img')).src = img
+}
+
+getImages()
+    .then(imgCreation)
+    .catch(reason=>console.log(reason))
